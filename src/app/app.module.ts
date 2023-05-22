55ada0e7af049dcf5cbdac9fd5e723e8e19f0ed5
Etienne Pogoda <pogoda.etienne@outlook.fr>
2023-05-22 16:18:59 +0200

    ci: add interceptors for differents endpoints api and request error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ApiInterceptor } from './core/interceptors/api.interceptor';
+import { ErrorInterceptor } from './core/interceptors/error.interceptor';
+import { HTTP_INTERCEPTORS, HttpClient, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,9 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [{ provide: HTTP_INTERCEPTORS, useClass: ApiInterceptor, multi: true }, { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/environments/environments.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    search_location_api_endpoint: "https://nominatim.openstreetmap.org/search?",
+    current_location_api_endpoint: "https://nominatim.openstreetmap.org/search?",
+    weather_api_endpoint: "https://api.open-meteo.com/v1/forecast?"
+}
