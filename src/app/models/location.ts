3b65e7247cba02545c0cf4afa5f07db48590f277
saloi sbai <saloi.benoum@gmail.com>
2023-05-22 14:48:14 +0200

    ci: models creation, location and weathers for the corresponding APIs

--- src/app/models/location.ts
+++ src/app/models/location.ts
@@ -1,2 +1,15 @@
 export interface Location {
+  place_id: number;
+  licence: string;
+  osm_type: string;
+  osm_id: number;
+  boundingbox: string[];
+  lat: string;
+  lon: string;
+  display_name: string;
+  place_rank: number;
+  category: string;
+  type: string;
+  importance: number;
+  icon: string;
 }
--- src/app/models/weather.ts
+++ src/app/models/weather.ts
@@ -1,2 +1,61 @@
 export interface Weather {
+  latitude: number;
+  longitude: number;
+  generationtime_ms: number;
+  utc_offset_seconds: number;
+  timezone: string;
+  timezone_abbreviation: string;
+  elevation: number;
+  current_weather: CurrentWeather;
+  hourly_units: HourlyUnits;
+  hourly: Hourly;
+  daily_units: DailyUnits;
+  daily: Daily;
+}
+
+export interface CurrentWeather {
+  temperature: number;
+  windspeed: number;
+  winddirection: number;
+  weathercode: number;
+  is_day: number;
+  time: string;
+}
+
+export interface HourlyUnits {
+  time: string;
+  temperature_2m: string;
+  relativehumidity_2m: string;
+  windspeed_10m: string;
+  winddirection_10m: string;
+  weathercode: string;
+  visibility: string;
+  is_day: string;
+}
+
+export interface Hourly {
+  time: string[];
+  temperature_2m: number[];
+  relativehumidity_2m: number[];
+  windspeed_10m: number[];
+  winddirection_10m: number[];
+  weathercode: number[];
+  visibility: number[];
+  is_day: number[];
+}
+
+export interface DailyUnits {
+  time: string;
+  sunrise: string;
+  sunset: string;
+  uv_index_max: string;
+  uv_index_clear_sky_max: string;
+}
+
+export interface Daily {
+  time: string[];
+  sunrise: string[];
+  sunset: string[];
+  uv_index_max: number[];
+  uv_index_clear_sky_max: number[];
 }
