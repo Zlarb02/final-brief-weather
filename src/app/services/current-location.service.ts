e641ede3c63dfbba1a160042da734d873a20dae2
saloi sbai <saloi.benoum@gmail.com>
2023-05-23 10:38:48 +0200

    ci: add images, experiment services

--- src/app/services/current-location.service.ts
+++ src/app/services/current-location.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Location } from '../models/location';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CurrentLocationService {
 
-  constructor() { }
+  
+  getLatAndLonFromCityName(Location: string): Observable<Location[]> {
+    // https://nominatim.openstreetmap.org/search.php?q=paris&format=jsonv2
+
+    return this.http.get<Location[]>(
+      'https://nominatim.openstreetmap.org/search.php?q=' +
+        Location +
+        '&format=jsonv2'
+    );
+  }
 }
