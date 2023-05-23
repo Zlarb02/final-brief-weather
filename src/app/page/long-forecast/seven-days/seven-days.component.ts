85753b11cef2b674fc6e6e4e1ea5604a9b2ceb24
Etienne Pogoda <pogoda.etienne@outlook.fr>
2023-05-23 15:44:06 +0200

    ci: add bootstrap, display dates in seven-days component

--- src/app/page/long-forecast/seven-days/seven-days.component.ts
+++ src/app/page/long-forecast/seven-days/seven-days.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { WeatherService } from 'src/app/services/weather.service';
 
 @Component({
   selector: 'app-seven-days',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./seven-days.component.scss']
 })
 export class SevenDaysComponent {
+  @Input() currentLocation: any | undefined;
+  @Input() public chosenLocation: any | undefined;
+  @Input() public weather: any | undefined;
+  public dates!: string[];
 
+  constructor(private weatherService: WeatherService) { }
+
+
+  getSevenDaysWeather() {
+    this.weatherService.getWeatherForecast(this.currentLocation.lat, this.currentLocation.lon)
+      .subscribe(
+        (weather) => {
+          return this.dates = weather.daily.time;
+        },
+        (error) => {
+          console.error(error);
+        }
+      );
+  }
 }
