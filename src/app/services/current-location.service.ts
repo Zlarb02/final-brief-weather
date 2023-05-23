b841b74ed112a1c4d9f195e6c45f93d22fc78c50
saloi sbai <saloi.benoum@gmail.com>
2023-05-23 11:00:39 +0200

    ref: remove import

--- src/app/services/current-location.service.ts
+++ src/app/services/current-location.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Location } from '../models/location';
+
 
 @Injectable({
   providedIn: 'root',
