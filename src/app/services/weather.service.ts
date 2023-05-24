4a7750cd1abe0b325ca19c9edb42c6fb2f2aa8b6
Etienne Pogoda <pogoda.etienne@outlook.fr>
2023-05-24 10:24:23 +0200

    update weather request and model with all params possible

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -11,7 +11,8 @@ export class WeatherService {
   constructor(private http: HttpClient) { }
 
   getWeatherForecast(latitude: number, longitude: number): Observable<Weather> {
-    const url = `weather_api_endpoint?latitude=${latitude}&longitude=${longitude}&timezone=auto&daily=sunrise,sunset,uv_index_max,uv_index_clear_sky_max,weathercode&hourly=temperature_2m,relativehumidity_2m,windspeed_10m,winddirection_10m,weathercode,visibility,is_day&current_weather=true`
+    const url = `weather_api_endpoint?latitude=${latitude}&longitude=${longitude}&elevation=44.812&hourly=temperature_2m,relativehumidity_2m,dewpoint_2m,apparent_temperature,pressure_msl,surface_pressure,cloudcover,cloudcover_low,cloudcover_mid,cloudcover_high,windspeed_10m,windspeed_80m,windspeed_120m,windspeed_180m,winddirection_10m,winddirection_80m,winddirection_120m,winddirection_180m,windgusts_10m,shortwave_radiation,direct_radiation,direct_normal_irradiance,diffuse_radiation,vapor_pressure_deficit,cape,evapotranspiration,et0_fao_evapotranspiration,precipitation,snowfall,precipitation_probability,rain,showers,weathercode,snow_depth,freezinglevel_height,visibility,soil_temperature_0cm,soil_temperature_6cm,soil_temperature_18cm,soil_temperature_54cm,soil_moisture_0_1cm,soil_moisture_1_3cm,soil_moisture_3_9cm,soil_moisture_9_27cm,soil_moisture_27_81cm,is_day&daily=temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum,rain_sum,showers_sum,snowfall_sum,precipitation_hours,precipitation_probability_max,precipitation_probability_min,precipitation_probability_mean,weathercode,sunrise,sunset,windspeed_10m_max,windgusts_10m_max,winddirection_10m_dominant,shortwave_radiation_sum,et0_fao_evapotranspiration,uv_index_max,uv_index_clear_sky_max&current_weather=true&temperature_unit=celsius&windspeed_unit=kmh&precipitation_unit=mm&timezone=auto&forecast_days=7`;
+
     return this.http.get<Weather>(url);
   }
 
